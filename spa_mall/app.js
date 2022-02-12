92f135be5bcd63465f2e09d8567254f30bb8dd6b
Seungmin <haradwaith@naver.com>
2022-02-13 01:21:28 +0900

    [UPDATE] 과제 전 etc

--- spa_mall/app.js
+++ spa_mall/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
+const connect = require('./schemas')
+// const connect = requrie('./schemas')로 작성하여도 된다.
+// 저런 식으로 쓰면, schemas라는 js파일을 먼저 찾고, 폴더를 찾은 후 
+// 'index.js'를 찾아본다.
 const res = require('express/lib/response');
 const app = express(); // express 서버 객체를 받아온다.
 const port = 3000;
+
+connect();
+
 // require은 package를 불러오는 함수이다.
-const goodsRouter = require("./routes/goods.js");
+const goodsRouter = require("./routes/goods");
+const cartsRouter = require('./routes/carts')
 // .js는 생략 가능
 
 const requestMiddleware = (req, res, next) => {
     console.log("Request URL :", req.originalUrl, " - ", new Date());
     next();
 }
+
+app.use(express.json());
 
 app.use(requestMiddleware);
 /*
@@ -38,8 +48,8 @@ app.use((req, res, next) => {
 // 받을 수 있다.
 */
 
-app.use("/api", goodsRouter)
-// /api로 들어왔을 떄, goodsRouter의 함수가 실행된다.
+app.use("/api", [goodsRouter, cartsRouter])
+// /api로 들어왔을 떄, [goodsRouter, cartsRouter]의 함수가 실행된다.
 // 뒤에 함수를 [f1, f2]로 설정하여 각각에 대해 검사도 가능하다.
 // 여기선, /api로 들어왔을 때 해당 뒷 부분에 따라서 goodsRouter가 동작한다.
 // 자세한 이해를 위해서는 goods.js 파일을 다시 보면 된다.
--- spa_mall/routes/goods.js
+++ spa_mall/routes/goods.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart")
 const router = express.Router();
 //const router = require("express").Router(); 이런 방식도 가능하다.
 
@@ -6,55 +8,94 @@ router.get("/", (req, res) => {
     res.send("this is root page");
 });
 
-const goods = [
-    {
-        goodsId: 4,
-        name: "상품 4",
-        thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-        category: "drink",
-        price: 0.1,
-    },
-    {
-        goodsId: 3,
-        name: "상품 3",
-        thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-        category: "drink",
-        price: 2.2,
-    },
-    {
-        goodsId: 2,
-        name: "상품 2",
-        thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-        category: "drink",
-        price: 0.11,
-    },
-    {
-        goodsId: 1,
-        name: "상품 1",
-        thumbnailUrl:
-        "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-        category: "drink",
-        price: 6.2,
-    },
-    ];
+// 목록 조회
+router.get("/goods", async (req, res) => {
+    // async 함수와 await를 이용해야 한다. why? promise 객체이다.
+    const { category } = req.query;
 
-router.get("/goods", (req, res) => {
+    console.log('category?', category);
+
+    const goods = await Goods.find({ category });
+
     res.json({
-        goods
+        goods,
     });
 });
 
-// /goods/abcd나, /goods/1234
-router.get("/goods/:goodsId", (req, res) => {
-    const goodsId = req.params.goodsId;
-    const [detail] = goods.filter((item) => item.goodsId === Number(goodsId));
+// 상세 조회
+router.get("/goods/:goodsId", async (req, res) => {
+    // /goods/abcd나, /goods/1234 형태
+    const { goodsId } = req.params;
+
+    const [detail] = await Goods.find({ goodsId: Number(goodsId) });
 
     res.json({
-        detail
+        detail,
     });
+});
+
+router.post("/goods/:goodsId/cart", async (req, res) => {
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
+    // header는 string type으로, body는 json 그대로 받는다.
+
+    const existCarts = await Cart.find({ goodsId: Number(goodsId) });
+    if (existCarts.length) {
+        return res.status(400).json({ success: false, errorMessage: "이미 장바구니에 들어 있는 상품입니다." })
+    }
+    await Cart.create({ goodsId: Number(goodsId), quantity });
+    res.json({ success: true })
+});
+
+router.delete("/goods/:goodsId/cart", async (req, res) => {
+    const { goodsId } = req.params;
+    const existCarts = await Cart.find({ goodsId: Number(goodsId) });
+
+    if (existCarts.length) {
+        await Cart.deleteOne({ goodsId: Number(goodsId) });
+    }
+    res.json({ success: true });
+});
+
+router.put("/goods/:goodsId/cart", async (req, res) => {
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
+
+    if (quantity < 1) {
+        return res.json({ success: false, errorMessage: "수량은 음수가 될 수 없습니다." })
+    }
+
+    const existCarts = await Cart.find({ goodsId: Number(goodsId) });
+    if (!existCarts.length) {
+        return res.status(400).json({ success: false, errorMessage: "장바구니에 해당 상품이 없습니다." })
+    }
+
+    await Cart.updateOne({ goodsId: Number(goodsId) }, { $set: { quantity } });
+
+    res.json({ success: true });
+});
+
+// 상품 정보 생성
+router.post("/goods", async (req, res) => {
+    const { goodsId, name, thumbnailUrl, category, price } = req.body;
+
+    const goods = await Goods.find({ goodsId });
+
+    if (goods.length) {
+        return res
+            .status(400)
+            .json({ success: false, errorMessage: "이미 있는 데이터입니다." });
+    }
+
+    const createdGoods = await Goods.create({
+        goodsId,
+        name,
+        thumbnailUrl,
+        category,
+        price,
+    });
+
+    res.json({ goods: createdGoods });
 });
 
 module.exports = router;
